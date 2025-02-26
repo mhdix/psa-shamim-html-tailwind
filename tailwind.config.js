4e78208ac5087dfb9f941e7dcd6a972b5a4879f2
mhdix <domanloomu@gmail.com>
2025-02-26 08:05:12 -0300

    feature: Implementing a color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   content: ["./*.html"], // Ensure Tailwind scans your HTML files
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        base: '#000107',
+        primary: '#64A0FF',
+        "blue-100": "#050A24",
+        "blue-100": "#326CED",
+        white: '#FFFFFF',
+        gray: '#9D9D9D',
+        "gray-200":'#606060',
+        "gray-600": '#434343',
+        "gray-transparent": "rgba(32, 32, 32, 0.5)",
+      }
+    },
   },
-  plugins: [],
+  plugins: [],  
 };
